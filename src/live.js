7b18efe59d7f581c17827b95d84d13f68721552b
Jason Stiebs <jason@rokkincat.com>
2018-01-11 19:37:35 -0600

    Adding Cover Art Support, as well as returning music brainz meta data just in case

--- src/live.js
+++ src/live.js
@@ -4,14 +4,14 @@ var devnull = require('dev-null');
 var musicbrainz = require('./musicbrainz');
 
 module.exports = {
-  "local": new EventEmitter(),
-  "all": new EventEmitter()
+    "local": new EventEmitter(),
+    "all": new EventEmitter()
 }
 
 var PIPES = {}
 const URLS = {
-  "local": "http://wyms.streamguys1.com/414music_aac",
-  "all": "https://wyms.streamguys1.com/live"
+    "local": "http://wyms.streamguys1.com/414music_aac",
+    "all": "https://wyms.streamguys1.com/live"
 }
 
 function decodeHTMLEntities(text) {
@@ -28,65 +28,114 @@ function decodeHTMLEntities(text) {
         ['quot', '"']
     ];
 
-    for (var i = 0, max = entities.length; i < max; ++i) 
-        text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+    for (var i = 0, max = entities.length; i < max; ++i)
+        text = text.replace(new RegExp('&' + entities[i][0] + ';', 'g'), entities[i][1]);
 
     return text;
 }
 
 
 function parseMetadata(metadata) {
-  var regex = /StreamTitle\='(.+?) - (.+?)';/;
-  var matches = metadata.match(regex);
-  if(matches) {
-    return {
-      artist: decodeHTMLEntities(matches[1]),
-      track: decodeHTMLEntities(matches[2])
+    var regex = /StreamTitle\='(.+?) - (.+?)';/;
+    var matches = metadata.match(regex);
+    if (matches) {
+        return {
+            artist: decodeHTMLEntities(matches[1]),
+            track: decodeHTMLEntities(matches[2])
+        }
+    } else {
+        return null;
     }
-  } else {
-    return null;
-  }
 }
 
 Object.keys(module.exports).forEach((key) => {
-  module.exports[key].on('newListener', (event) => {
-    if(event !== 'song') return;
-    if(module.exports[key].listenerCount('song') == 0) {
-      icy.get(URLS[key], (res) => {
-        res.on("metadata", (metadata) => {
-          metadata = parseMetadata(metadata.toString());
-          if(metadata != null && metadata.artist != '414 Music') {
-            musicbrainz.artist(metadata.artist).then((artistObj) => {
-              return musicbrainz.track(metadata.track, artistObj).then((track) => {
-                module.exports[key].emit('song', {
-                  "artist": artistObj.name,
-                  "track": track.title,
-                  "album": track.releases[0].title,
-                  "playedAt": new Date()
+    module.exports[key].on('newListener', (event) => {
+        if (event !== 'song') return;
+        if (module.exports[key].listenerCount('song') == 0) {
+            icy.get(URLS[key], (res) => {
+                res.on("metadata", (metadata) => {
+                    metadata = parseMetadata(metadata.toString());
+                    if (metadata != null && metadata.artist != '414 Music') {
+                        getMusicbrainz(metadata).then((obj) => {
+                            module.exports[key].emit('song', obj);
+                        })
+                    }
                 });
-              })
-            }).catch((e) => {
-              module.exports[key].emit('song', {
-                "artist": metadata.artist,
-                "track": metadata.track,
-                "album": null,
-                "playedAt": new Date()
-              });
-            });
-          }
-        })
-        res.pipe(devnull());
-        PIPES[key] = res;
-      })
-    }
-  })
-  module.exports[key].on('removeListener', () => {
-    if(module.exports.listenerCount('song') == 1) {
-      if(PIPES[key]) {
-        PIPES[key].unpipe(devnull());
-        PIPES[key] = null;
-      }
-    }
-  });
+                res.pipe(devnull());
+                PIPES[key] = res;
+            })
+        }
+    })
+    module.exports[key].on('removeListener', () => {
+        if (module.exports.listenerCount('song') == 1) {
+            if (PIPES[key]) {
+                PIPES[key].unpipe(devnull());
+                PIPES[key] = null;
+            }
+        }
+    });
 })
 
+
+function getMusicbrainz(metadata) {
+    let defaultObject = {
+        "artist": metadata.artist,
+        "track": metadata.track,
+        "album": null,
+        "playedAt": new Date(),
+        "coverArt": null,
+        "musicBrainz": {
+            "artist": null,
+            "track": null,
+            "cover": null,
+            "error": null
+        }
+    };
+
+
+    let buildObject = (artistObj, track, cover) => {
+        let builder = {
+            "playedAt": new Date(),
+            musicBrainz: {artist: null, track: null, cover: null, error: null}
+        };
+
+        if(artistObj) {
+            builder["artist"] = artistObj.name;
+            builder["musicBrainz"]["artist"] = artistObj;
+        }
+
+        if(track) {
+            let release = musicbrainz.getBestRelease(track);
+            builder["track"] = track.title;
+            builder["album"] = release.title;
+            builder["musicBrainz"]["track"] = track;
+        }
+
+        if(cover) {
+            builder["cover"] = cover.image
+            builder["musicBrainz"]["cover"] = cover;
+        }
+
+        return Object.assign(defaultObject, builder);
+    };
+
+    return new Promise((resolve, reject) => {
+        musicbrainz.artist(metadata.artist).then((artistObj) => {
+            if(artistObj) {
+                musicbrainz.track(metadata.track, artistObj).then((track) => {
+                    if(track) {
+                        musicbrainz.coverArt(track).then((cover) => {
+                            resolve(buildObject(artistObj, track, cover));
+                        });
+                    } else {
+                        resolve(buildObject(artistObj, track, null));
+                    }
+                });
+            } else {
+                resolve(buildObject(artistObj, null, null));
+            }
+        });
+    }).catch((e) => {
+        resolve(Object.assign(defaultObject, {"musicBrainz": {"error": e}}));
+    });
+}
--- src/musicbrainz.js
+++ src/musicbrainz.js
@@ -16,10 +16,7 @@ exports.artist = (name) => {
     })
   }).then((artists) => {
     if(artists.artists.length > 0) {
-      return {
-        id: artists.artists[0].id,
-        name: artists.artists[0].name
-      }
+      return artists.artists[0]
     } else {
       return null;
     }
@@ -38,9 +35,96 @@ exports.track = (name, artist) => {
     }, (response) => {
       var body = "";
       response.on('data', (chunk) => body += chunk)
-      response.on('end', () => resolve(JSON.parse(body)))
+      response.on('end', () => resolve(JSON.parse(body)));
     })
   }).then((json) => {
-    return json.recordings[0]
+      let sortedRecordings = json.recordings.sort((a, b) => a.score - b.score);
+      return sortedRecordings[sortedRecordings.length-1];
   });
 }
+
+
+exports.coverArt = (recording) => {
+    let release = getBestRelease(recording)["release-group"]["id"];
+    if(release) {
+        return new Promise((resolve, reject) => {
+            http.get({
+                protocol: "http:",
+                hostname: "coverartarchive.org",
+                headers: {
+                    "User-Agent": "Application 88nine/0.1.0 (radiomilwaukee.org)",
+                    "Accept": "application/json"
+                },
+                path: `/release-group/${release}`,
+            }, (response) => {
+                if(response.statusCode == 200) {
+                    var body = "";
+                    response.on('data', (chunk) => body += chunk)
+                    response.on('end', () => resolve(JSON.parse(body)))
+                } else {
+                    resolve(null)
+                }
+            })
+        }).then((json) => {
+            return json
+        });
+    } else {
+        return new Promise((resolve, reject) => resolve({}))
+    }
+}
+
+
+function getBestRelease(recording) {
+    // Group releases. If there is a common release group use that.
+    let groupedReleases = recording.releases.reduce((acc, i) => {
+        let rg = i["release-group"];
+        if(rg) {
+            let val = acc[rg.id] || 0;
+            acc[rg.id]++;
+            return acc;
+        }
+    }, {});
+
+    let release = null;
+    let maxLength = 0;
+
+    // Use the most commonly used group.
+    for (var key in groupedReleases ) {
+        let val = groupedReleases[key];
+        if(val > maxLength && val != 1) {
+            release = recording.releases.find((i) => {
+                let rg = i["release-group"];
+                return rg && rg["id"] == key;
+            });
+        }
+    }
+
+    // If we don't have a common release group, find the first official one with type EP
+    if(!release) {
+        release = recording.releases.find(function(i){
+            let rg = i["release-group"];
+            return i["status"] == 'Official' && rg && rg["primary-type"] == "EP";
+        });
+
+    }
+
+    // If not try to find the first Official and US release with a group
+    if(!release) {
+        release = recording.releases.find(function(i){
+            let rg = i["release-group"];
+            return rg && i["status"] == 'Official' && i['country'] == 'US';
+        });
+    }
+
+    // Get the first release group we have
+    if(!release) {
+        release = recording.releases.find((i)=> {
+            let rg = i["release-group"];
+            return !!rg;
+        });
+    }
+
+    return release;
+}
+
+exports.getBestRelease = getBestRelease;
